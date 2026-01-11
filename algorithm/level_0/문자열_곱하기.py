2270d0be6af22693505d12619400e6b615b81647
JEEYEON LEE <joeuni94@gmail.com>
2026-01-11 13:24:02 +0900

    Create 문자열_곱하기.py

---
+++ algorithm/level_0/문자열_곱하기.py
@@ -0,0 +1,8 @@
+# URL: https://school.programmers.co.kr/learn/courses/30/lessons/181940
+# 요약: (입력) love, 10 -> (출력) lovelovelovelovelovelovelovelovelovelove 
+
+
+# ================================================================
+# 내 풀이
+def solution(a, k):
+    return a*k
